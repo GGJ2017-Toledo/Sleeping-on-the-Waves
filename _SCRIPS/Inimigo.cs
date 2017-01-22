278ea4cef549c692919ae2ed6e524a683a2cc27a
Valdecir Ribeiro R. Sutil <valdecirribeiro.25@hotmail.com>
2017-01-22 18:11:09 -0200

    Add files via upload

---
+++ _SCRIPS/Inimigo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+    private int playerDanger;
+    private Animation animator;
+    private Transform target;
+    private bool moveSkikpe;
+
+
+	void Start () {
+        animator = GetComponent<Animation>();
+      
+    }
+	
+	
+	void Update () {
+	
+	}
+}
